0ce73a883c1f387b16eda370456fc7e652467c35
MohdAfix <linexsg@gmail.com>
2026-05-20 22:37:36 +0800

    build-optimization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,8 +78,6 @@ android {
         ndk {
             abiFilters.add("armeabi-v7a")
             abiFilters.add("arm64-v8a")
-            abiFilters.add("x86_64")
-            abiFilters.add("x86")
         }
 
         externalNativeBuild {
@@ -116,7 +114,8 @@ android {
             }
         }
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
