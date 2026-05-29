d5f0b7e073bf34cec2c99973837ebb49d9f491e8
MohdAfix <linexsg@gmail.com>
2026-05-29 16:41:53 +0800

    fix(call_info): add try-catch around IP lookup and append error message

--- app/src/main/java/com/wmods/wppenhacer/xposed/features/general/Others.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/features/general/Others.java
@@ -407,39 +407,42 @@ public class Others extends Feature {
         sb.append(String.format(Utils.getApplication().getString(R.string.phone_number_s), number)).append("\n");
         var ip = (String) XposedHelpers.getObjectField(wamCall, "callPeerIpStr");
         if (ip != null) {
-            var client = new OkHttpClient.Builder().build();
-            var url = "http://ip-api.com/json/" + ip;
-            var request = new okhttp3.Request.Builder().url(url).build();
-            var content = client.newCall(request).execute().body().string();
-            var json = new JSONObject(content);
-            
             sb.append(String.format(Utils.getApplication().getString(R.string.ip_s), ip)).append("\n");
-            
-            if ("success".equalsIgnoreCase(json.optString("status"))) {
-                var country = json.optString("country");
-                var countryCode = json.optString("countryCode");
-                var regionName = json.optString("regionName");
-                var region = json.optString("region");
-                var city = json.optString("city");
-                var zip = json.optString("zip");
-                var timezone = json.optString("timezone");
-                var isp = json.optString("isp");
-                var org = json.optString("org");
-                var asStr = json.optString("as");
-                var lat = json.optString("lat");
-                var lon = json.optString("lon");
+            try {
+                var client = new OkHttpClient.Builder().build();
+                var url = "http://ip-api.com/json/" + ip;
+                var request = new okhttp3.Request.Builder().url(url).build();
+                var content = client.newCall(request).execute().body().string();
+                var json = new JSONObject(content);
                 
-                if (!TextUtils.isEmpty(country)) sb.append(String.format(Utils.getApplication().getString(R.string.country_s), country)).append("\n");
-                if (!TextUtils.isEmpty(countryCode)) sb.append(String.format(Utils.getApplication().getString(R.string.country_code_s), countryCode)).append("\n");
-                if (!TextUtils.isEmpty(regionName)) sb.append(String.format(Utils.getApplication().getString(R.string.region_name_s), regionName)).append("\n");
-                if (!TextUtils.isEmpty(region)) sb.append(String.format(Utils.getApplication().getString(R.string.region_s), region)).append("\n");
-                if (!TextUtils.isEmpty(city)) sb.append(String.format(Utils.getApplication().getString(R.string.city_s), city)).append("\n");
-                if (!TextUtils.isEmpty(zip)) sb.append(String.format(Utils.getApplication().getString(R.string.zip_s), zip)).append("\n");
-                if (!TextUtils.isEmpty(timezone)) sb.append(String.format(Utils.getApplication().getString(R.string.timezone_s), timezone)).append("\n");
-                if (!TextUtils.isEmpty(isp)) sb.append(String.format(Utils.getApplication().getString(R.string.isp_s), isp)).append("\n");
-                if (!TextUtils.isEmpty(org)) sb.append(String.format(Utils.getApplication().getString(R.string.org_s), org)).append("\n");
-                if (!TextUtils.isEmpty(asStr)) sb.append(String.format(Utils.getApplication().getString(R.string.as_s), asStr)).append("\n");
-                if (!TextUtils.isEmpty(lat) && !TextUtils.isEmpty(lon)) sb.append(String.format(Utils.getApplication().getString(R.string.lat_lon_s), lat, lon)).append("\n");
+                if ("success".equalsIgnoreCase(json.optString("status"))) {
+                    var country = json.optString("country");
+                    var countryCode = json.optString("countryCode");
+                    var regionName = json.optString("regionName");
+                    var region = json.optString("region");
+                    var city = json.optString("city");
+                    var zip = json.optString("zip");
+                    var timezone = json.optString("timezone");
+                    var isp = json.optString("isp");
+                    var org = json.optString("org");
+                    var asStr = json.optString("as");
+                    var lat = json.optString("lat");
+                    var lon = json.optString("lon");
+                    
+                    if (!TextUtils.isEmpty(country)) sb.append(String.format(Utils.getApplication().getString(R.string.country_s), country)).append("\n");
+                    if (!TextUtils.isEmpty(countryCode)) sb.append(String.format(Utils.getApplication().getString(R.string.country_code_s), countryCode)).append("\n");
+                    if (!TextUtils.isEmpty(regionName)) sb.append(String.format(Utils.getApplication().getString(R.string.region_name_s), regionName)).append("\n");
+                    if (!TextUtils.isEmpty(region)) sb.append(String.format(Utils.getApplication().getString(R.string.region_s), region)).append("\n");
+                    if (!TextUtils.isEmpty(city)) sb.append(String.format(Utils.getApplication().getString(R.string.city_s), city)).append("\n");
+                    if (!TextUtils.isEmpty(zip)) sb.append(String.format(Utils.getApplication().getString(R.string.zip_s), zip)).append("\n");
+                    if (!TextUtils.isEmpty(timezone)) sb.append(String.format(Utils.getApplication().getString(R.string.timezone_s), timezone)).append("\n");
+                    if (!TextUtils.isEmpty(isp)) sb.append(String.format(Utils.getApplication().getString(R.string.isp_s), isp)).append("\n");
+                    if (!TextUtils.isEmpty(org)) sb.append(String.format(Utils.getApplication().getString(R.string.org_s), org)).append("\n");
+                    if (!TextUtils.isEmpty(asStr)) sb.append(String.format(Utils.getApplication().getString(R.string.as_s), asStr)).append("\n");
+                    if (!TextUtils.isEmpty(lat) && !TextUtils.isEmpty(lon)) sb.append(String.format(Utils.getApplication().getString(R.string.lat_lon_s), lat, lon)).append("\n");
+                }
+            } catch (Exception e) {
+                sb.append("IP Lookup Error: ").append(e.getMessage()).append("\n");
             }
         }
         var platform = (String) XposedHelpers.getObjectField(wamCall, "callPeerPlatform");
